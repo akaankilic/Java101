a741c4f8d897107feeb260cd42b98bcb939d6ce7
Adil Kaan Kilic <147434903+akaankilic@users.noreply.github.com>
2023-11-27 00:01:01 +0300

    Kdv Hesaplama
    Ürün vergi hesaplama

---
+++ KdvHesaplama/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Satın Almak İstediğiniz Ürünün Fiyatını Giriniz..");
+        Scanner input=new Scanner(System.in);
+        System.out.println("1000 Tl aldındaki ürünlerde kdv %18, üzerinde ise %8\'dir");
+        double tutar=input.nextDouble();
+        double kdv=1.18;
+        double dusukKdv=1.08;
+        System.out.println("Kdvsiz Fiyat="+tutar);
+        System.out.println(tutar<1000 ? "Kdv oranı =%18":"Kdv Oranı %8");
+        System.out.println(tutar<1000 ? "Kdv Dahil fiyat"+tutar*kdv:"Kdv Dahil fiyat"+tutar*dusukKdv);
+
+
+
+    }
+}
