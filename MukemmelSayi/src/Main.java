ae057417f3deb32bd1a855d3b66d9bf60c3a381e
Adil Kaan Kilic <147434903+akaankilic@users.noreply.github.com>
2023-12-04 21:12:15 +0300

    update

--- MukemmelSayi/src/Main.java
+++ MukemmelSayi/src/Main.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Mükemmel olup olmadığını öğrenmek istediğiniz sayıyı giriniz : ");
-        Scanner input =new Scanner(System.in);
-        int number=input.nextInt();
-        int sayac=0;
-        for (int i=1;i<=number/2;i++){
-            if (number%i==0){
-                sayac+=i;
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int sayac = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sayac += i;
             }
         }
-        if (number==sayac){
+        if (number == sayac) {
             System.out.println("Sayınız mükemmel bir sayıdır.");
-        }else {
+        } else {
             System.out.println("Sayınız mükemmel bir sayı değildir.");
         }
     }
