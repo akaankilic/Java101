6ac5ca1c1788bb1f0bd4b5b04360ea8776c73635
Adil Kaan Kilic <147434903+akaankilic@users.noreply.github.com>
2023-12-09 22:45:57 +0300

    YakinDeger
    En yakın değerleri bulma

---
+++ YakinDeger/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] list = {15, 12, 788, 1, -1, -778, 2, 0};
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir değer giriniz :");
+        int a = input.nextInt();
+        System.out.println("Dizi = "+Arrays.toString(list));
+        Arrays.sort(list);
+        System.out.println("Girilen Değer : "+a);
+        int kucukYakin=a;
+        int temp=a;
+        int gecici;
+        int buyukYakin=list[list.length-1];
+        for (int i:list){
+            if (i<a){
+                kucukYakin=i;
+            }
+        }
+        for (int i:list)
+            if (i>a){
+                temp=i;
+                gecici=temp;
+                if (gecici>a&&gecici<buyukYakin){
+                    buyukYakin=gecici;
+                }
+            }
+
+        System.out.println("Girilen sayıdan küçük en yakın sayı : "+kucukYakin);
+        System.out.println("Girilen sayıdan büyük en yakın sayı : "+buyukYakin);
+
+    }
+
+
+}
