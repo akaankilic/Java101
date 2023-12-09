05c204357e7ccfb16269a378440da50b0ff6e593
Adil Kaan Kilic <147434903+akaankilic@users.noreply.github.com>
2023-12-09 18:26:43 +0300

    vergi ve artış güncellemesi

--- Fabrika/src/Employee.java
+++ Fabrika/src/Employee.java
@@ -18,41 +18,46 @@ public class Employee {
         this.workHours=workHours;
         this.hireYear=hireYear;
     }
-    public double tax(){
+    public void tax(){
         if (this.salary<1000){
             System.out.println("Vergisiz Dilimde Çalışıyor");
-        } else if (this.salary>1000) {
-            double vergi=this.salary*0.03;
-            this.salary-=this.salary-vergi;
         }
-        return vergi;
+        if (this.salary>1000) {
+           this.vergi=this.salary*0.03;
+            System.out.println("Çalışan vergisi : "+this.vergi);
+        }
+
     }
-    public int bonus(){
+    public void bonus(){
         if (this.workHours>=40){
             this.bonus=(this.workHours-40)*30;
+            System.out.println("Çalışan bonusu: "+this.bonus);
         }
-        return bonus;
     }
 
     public void raiseSalary(){
         LocalDateTime ldt = LocalDateTime.now();
         this.yıl=ldt.getYear()-this.hireYear;
-        if (this.yıl>20){
-                this.zam=this.salary*0.3;
-        } else if (this.yıl<20&&this.yıl>10) {
-            this.zam=this.salary*0.1;
-        } else if (this.yıl<10) {
-            this.zam=this.salary*0.05;
+        if (this.yıl>=20){
+            this.zam=(this.salary*0.15);
         }
+        if (this.yıl<=19&&this.yıl>=10) {
+            this.zam=(this.salary*0.1);
+        }
+        if (this.yıl<=10) {
+            this.zam=(this.salary*0.05);
+        }
+        System.out.println("Çalışan Maaş artışı : "+this.zam);
+
     }
     public String toString(){
         System.out.println("Çalışan : "+this.name);
         System.out.println("Çalışan Maaşı : "+this.salary);
         System.out.println("Haftalık Çalışma Süresi : "+this.workHours);
         System.out.println("Çalışan işe giriş yılı : "+this.hireYear);
-        System.out.println("Çalışan vergisi : "+this.vergi);
-        System.out.println("Çalışan bonusu: "+this.bonus);
-        System.out.println("Çalışan Maaş artışı : "+this.zam);
+        bonus();
+        tax();
+        raiseSalary();
         System.out.println("Vergi ve bonuslar ile Maaş: "+(this.salary-this.vergi+this.bonus));
         System.out.println("Toplam Maaş : "+(this.salary-this.vergi+this.bonus+this.zam));
         return this.name;
--- Fabrika/src/Main.java
+++ Fabrika/src/Main.java
@@ -3,7 +3,7 @@ import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        Employee calisan=new Employee("ahmet",1000,53,2003);
+        Employee calisan=new Employee("ahmet",2000,53,1999);
         calisan.toString();
 
     }
