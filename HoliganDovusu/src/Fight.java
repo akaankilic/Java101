ebf4d31f603d32f98ed4b019f248b8e4013bd149
Adil Kaan Kilic <147434903+akaankilic@users.noreply.github.com>
2023-12-09 16:59:39 +0300

    HoliganDovusu
    Benekli Ayhan vs Rambo Okan

---
+++ HoliganDovusu/src/Fight.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class Fight {
+    Holigan h1;
+    Holigan h2;
+    int minWeight;
+    int maxWeight;
+
+    Fight(Holigan h1, Holigan h2, int maxWeight, int minWeight) {
+        this.h1 = h1;
+        this.h2 = h2;
+        this.maxWeight = maxWeight;
+        this.minWeight = minWeight;
+    }
+
+    void dovus() {
+        if (isCheck()) {
+            System.out.println("Çatışma Başladı..");
+            for (int i = 1; this.h1.health >= 0 && this.h2.health >= 0; i++) {
+                System.out.println(i + " . Round ");
+                Random d = new Random();
+                System.out.println("Saldıran--" + this.h1.name);
+                this.h1.damage = d.nextInt(this.h1.damage + 10);
+                System.out.println("Vurulan Hasar = " + this.h1.damage);
+                int hasar1 = this.h1.damage;
+                this.h2.health -= hasar1;
+                System.out.println(this.h2.name + " Kalan canı " + this.h2.health);
+                System.out.println("=============");
+                System.out.println("      ");
+                Random s = new Random();
+                System.out.println("Saldıran--" + this.h2.name);
+                this.h2.damage = s.nextInt(this.h2.damage + 10);
+                System.out.println("Vurulan Hasar = " + this.h2.damage);
+                int hasar2 = this.h2.damage;
+                this.h1.health -= hasar2;
+                this.h1.health = this.h1.health - this.h2.damage;
+                System.out.println(this.h1.name + " Kalan canı " + this.h1.health);
+                System.out.println("=============");
+                System.out.println("      ");
+                if (this.h1.health < 0||this.h2.health < 0) {
+                    System.out.println("Dövüş Sona Erdi");
+                    if (this.h1.health<0){
+                        System.out.println("Kazanan = " + this.h2.name);
+                    }
+                    if (this.h2.health<0){
+                        System.out.println("Kazanan = " + this.h1.name);
+                    }
+
+
+                }
+            }
+        } else {
+            System.out.println("Holiganlar Birbirine eş değil..");
+        }
+
+    }
+
+    boolean isCheck() {
+        return (this.h1.weight <= this.maxWeight && this.h1.weight >= this.minWeight) &&
+                (this.h2.weight <= this.maxWeight && this.h2.weight >= this.minWeight);
+    }
+}
---
+++ HoliganDovusu/src/Holigan.java
@@ -0,0 +1,16 @@
+public class Holigan {
+    String name;
+    String team;
+    int weight;
+    int health;
+    int damage;
+    Holigan(String name,String team,int weight,int health,int damage){
+        this.name=name;
+        this.team=team;
+        this.weight=weight;
+        this.health=health;
+        this.damage=damage;
+
+    }
+
+}
