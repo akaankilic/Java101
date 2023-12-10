6efc8d0fa02b80cc72f0c15e2433ac97907464fc
Adil Kaan Kilic <147434903+akaankilic@users.noreply.github.com>
2023-12-10 19:53:28 +0300

    MayinTarlası

---
+++ MayinTarlasi/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("**Mayın Tarlasına Hoşgeldin.**");
+        System.out.println("Oynamak istediğiniz genişlik ve uzunluğu giriniz\n==================================");
+        System.out.print("Uzunluk : ");
+        int row= input.nextInt();
+        System.out.print("Genişlik : ");
+        int col=input.nextInt();
+        MineSweeper mayin=new MineSweeper(row,col);
+        mayin.run();
+
+    }
+}
